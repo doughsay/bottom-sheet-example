b09cf6dd054945ee8d8b7a9025ab012091c5b0e1
Chris Dosé <chris@xn--dos-dma.com>
2024-11-18 16:19:52 -0800

    repro

--- App.tsx
+++ App.tsx
@@ -1,20 +1,59 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import {
+  BottomSheetModal,
+  BottomSheetModalProvider,
+  BottomSheetView,
+} from "@gorhom/bottom-sheet";
+import React, { useRef } from "react";
+import { Button, StyleSheet, Text, View } from "react-native";
+import { GestureHandlerRootView } from "react-native-gesture-handler";
 
-export default function App() {
+const App = () => {
+  const bottomSheetRef = useRef<BottomSheetModal>(null);
+
   return (
-    <View style={styles.container}>
-      <Text>Open up App.tsx to start working on your app!</Text>
-      <StatusBar style="auto" />
-    </View>
+    <GestureHandlerRootView>
+      <BottomSheetModalProvider>
+        <View style={styles.container}>
+          <Button
+            title="Present Modal"
+            onPress={() => bottomSheetRef.current?.present()}
+          />
+          <BottomSheetModal
+            ref={bottomSheetRef}
+            detached={true}
+            bottomInset={46}
+            style={styles.sheetContainer}
+          >
+            <BottomSheetView>
+              <View style={styles.contentContainer}>
+                <Text>Bottom</Text>
+                <Text>Sheet</Text>
+                <Text>Modal</Text>
+                <Text>🎉</Text>
+              </View>
+            </BottomSheetView>
+          </BottomSheetModal>
+        </View>
+      </BottomSheetModalProvider>
+    </GestureHandlerRootView>
   );
-}
+};
 
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
+    justifyContent: "center",
+    padding: 24,
+    backgroundColor: "grey",
+  },
+  sheetContainer: {
+    marginHorizontal: 24,
+  },
+  contentContainer: {
+    flex: 1,
+    alignItems: "center",
+    padding: 24,
   },
 });
+
+export default App;
